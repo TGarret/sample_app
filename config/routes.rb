bd8f13060efe3b3daadf640d310bffa3ef5ac4d8
Tim <tim.garret@inseec-france.com>
2017-10-30 18:45:48 +0100

    implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
 root 'static_pages#home'
   get '/help',	to: 'static_pages#help'	
   get '/about',	to: 'static_pages#about'	
@@ -8,5 +10,9 @@ root 'static_pages#home'
   get '/signup',	to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
+	
+get '/login',	to: 'sessions#new'
+post '/login',	to: 'sessions#create'
+delete '/logout',	to: 'sessions#destroy'
 
 end
